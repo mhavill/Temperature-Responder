9fd1ec8fb4f46e1678e334e48bb7816719af66a3
mhavill <mick@mickhavill.com.au>
2021-10-04 14:46:48 +1100

    IOT Cloud set-up

--- src/thingProperties.h
+++ src/thingProperties.h
@@ -6,15 +6,15 @@
 // const char *device = "Temperature01";
 // float temp01;
 
-// const char THING_ID[] = "a2956bd6-04df-489a-a2ec-88df4f9b73cc";
-// const char DEVICE_LOGIN_NAME[] = "cbc27fce-ca2f-49c7-a14b-9ad0ceed3175";
-// const char *device = "Temperature02";
-// float temp02;
+const char THING_ID[] = "a2956bd6-04df-489a-a2ec-88df4f9b73cc";
+const char DEVICE_LOGIN_NAME[] = "cbc27fce-ca2f-49c7-a14b-9ad0ceed3175";
+const char *device = "Temperature02";
+float temp02;
 
-const char THING_ID[] = "491ac29d-546c-4ee0-b643-fdc1eebe36f0";
-const char DEVICE_LOGIN_NAME[] = "e56cabf4-331b-4870-957a-13c770d2ee1e";
-const char *device = "Temperature03";
-float temp03;
+// const char THING_ID[] = "491ac29d-546c-4ee0-b643-fdc1eebe36f0";
+// const char DEVICE_LOGIN_NAME[] = "e56cabf4-331b-4870-957a-13c770d2ee1e";
+// const char *device = "Temperature03";
+// float temp03;
 
 // const char THING_ID[] = "0906ceb0-7557-4401-ae7f-5e5adf2293e9";
 // const char DEVICE_LOGIN_NAME[] = "7fff1822-9568-4a4f-8297-9b39a405cd58";
@@ -30,8 +30,8 @@ void setTemp(float temp)
 {
     // Set IOT Cloud variable
     // temp01 = temp;
-    // temp02 = temp;
-    temp03 = temp;
+    temp02 = temp;
+    // temp03 = temp;
     // temp04 = temp;
     //  temp05 = temp;
 }
@@ -43,8 +43,8 @@ void initProperties()
     ArduinoCloud.setSecretDeviceKey(DEVICE_KEY);
     ArduinoCloud.setThingId(THING_ID);
     // ArduinoCloud.addProperty(temp01, READ, 10 * SECONDS, NULL);
-    // ArduinoCloud.addProperty(temp02, READ, 10 * SECONDS, NULL);
-    ArduinoCloud.addProperty(temp03, READ, 10 * SECONDS, NULL);
+    ArduinoCloud.addProperty(temp02, READ, 10 * SECONDS, NULL);
+    // ArduinoCloud.addProperty(temp03, READ, 10 * SECONDS, NULL);
     // ArduinoCloud.addProperty(temp04, READ, 10 * SECONDS, NULL);
     //  ArduinoCloud.addProperty(temp05, READ, 10 * SECONDS, NULL);
 }
